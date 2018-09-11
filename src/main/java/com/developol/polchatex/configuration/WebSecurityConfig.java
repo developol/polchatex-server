90cdc08ef8a08a8f7f8eed3fcad80ec06eeb5bc2
Marek Baraniecki <czapol321@gmail.com>
2018-09-11 11:35:27 +0200

    runtime user registration added

--- src/main/java/com/developol/polchatex/configuration/WebSecurityConfig.java
+++ src/main/java/com/developol/polchatex/configuration/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.developol.polchatex.persistence.UserDtoRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Properties;
 
 
 @Configuration
@@ -43,9 +45,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable();
     }
 
-    @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         Iterable<UserDto> databaseUserList = userDtoRepository.findAll();
         LinkedList<UserDetails> securityUserList = new LinkedList<UserDetails>();
         Iterator i = databaseUserList.iterator();
@@ -60,9 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             .build()
             );
         }
-        return (new InMemoryUserDetailsManager(securityUserList));
+        return new InMemoryUserDetailsManager(securityUserList);
     }
-
-
 }
 
--- src/main/java/com/developol/polchatex/rest/MainController.java
+++ src/main/java/com/developol/polchatex/rest/MainController.java
@@ -3,23 +3,32 @@ import com.developol.polchatex.persistence.UserDto;
 import com.developol.polchatex.persistence.UserDtoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping(path="/rest")
 public class MainController {
 
     private UserDtoRepository userDtoRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
 
-    public MainController(UserDtoRepository userDtoRepository) {
+    public MainController(UserDtoRepository userDtoRepository,
+                          InMemoryUserDetailsManager inMemoryUserDetailsManager) {
         this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
         this.userDtoRepository = userDtoRepository;
+        this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
     }
 
+    @RequestMapping("exists/{username}")
+    public boolean userExists(@PathVariable("username") String username ) {
+        return inMemoryUserDetailsManager.userExists(username);
+    }
     @GetMapping(path="/hello")
     public @ResponseBody String greeting() {
         return "Hello World!";
@@ -38,6 +47,11 @@ public class MainController {
             userDto.setEmail("dupa");
 
             userDtoRepository.save(userDto);
+            inMemoryUserDetailsManager.createUser( User.builder()
+                    .username(userDto.getUsername())
+                    .password(userDto.getPassword())
+                    .roles("USER")
+                    .build());
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NO_CONTENT);
