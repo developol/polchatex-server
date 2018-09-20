d9981976bce16d81eb3aa44c230198d1850a5119
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-09-20 21:56:31 +0200

    properties for deploy

--- src/main/java/com/developol/polchatex/properties/Properties.java
+++ src/main/java/com/developol/polchatex/properties/Properties.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Properties {
-    private final String serverUrl = "http://localhost:8080";
-    //private final String serverUrl = "https://polchatex-server.herokuapp.com";
+    //private final String serverUrl = "http://localhost:8080";
+    private final String serverUrl = "https://polchatex-server.herokuapp.com";
 
-    //private final String serverDomain = "polchatex-server.herokuapp.com";
-    private final String serverDomain = "localhost";
-    private final String clientUrl = "http://localhost:4200";
-    //private final String clientUrl = "https://polchatex.herokuapp.com";
+    private final String serverDomain = "polchatex-server.herokuapp.com";
+    //private final String serverDomain = "localhost";
+    //private final String clientUrl = "http://localhost:4200";
+    private final String clientUrl = "https://polchatex.herokuapp.com";
 
     public String getServerUrl() {
         return serverUrl;
